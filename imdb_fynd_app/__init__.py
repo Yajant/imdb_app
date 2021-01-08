efb02ec1bb54ff0e97e7a4121ba2faf2c366613b
yajant <yajantbawsar9090@gmail.com>
2021-01-08 12:50:08 +0530

    modular

---
+++ imdb_fynd_app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'qwertyuiop'
+DB_NAME = 'IMDB'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}.db'.format(DB_NAME)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+
+@app.route('/', methods=['GET'])
+def test():
+    return 'Hello world'
